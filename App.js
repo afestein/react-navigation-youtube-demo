55a895ab4b32a80c64c9968b804ec3c113082e37
afestein <afestein@gmail.com>
2018-08-15 11:58:17 +1000

    Add basic YouTube example

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, { Component } from "react"
 import { Platform, StyleSheet, Text, View } from "react-native"
+import YouTube from "react-native-youtube"
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -21,6 +22,18 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <YouTube
+          apiKey="API_KEY_HERE"
+          videoId="527ZQgRx8W8" // The YouTube video ID
+          play={true} // control playback of video with true/false
+          fullscreen={false} // control whether the video should play in fullscreen or inline
+          loop={true} // control whether the video should loop when ended
+          onReady={e => this.setState({ isReady: true })}
+          onChangeState={e => this.setState({ status: e.state })}
+          onChangeQuality={e => this.setState({ quality: e.quality })}
+          onError={e => console.warn(e.error)}
+          style={{ width: 300, height: 200 }}
+        />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
